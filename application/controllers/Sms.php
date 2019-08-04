1ca4214c36b23c7737c92f73c667f3d594e917a2
jaimechacon <jaimechaconarevalo@gmail.com>
2019-08-04 19:00:26 -0400

    sitio de traspasos

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -39,13 +39,9 @@ class Sms extends CI_Controller {
 		if($json != null)
 		{
 			$data = json_decode($json);
-			$query = $this->Sms_model->saveSms($data->username, $data->password, $data->ani, $data->dnis, $data->message, $data->other_messages);
-			if($data != null && $data->username != null && $data->password != null)
+			if($data->username == "Sglo2019" && $data->username == "Sg.2019$$##")
 			{
-				if($data->username == "Sglo2019" && $data->username == "Sg.2019$$##")
-				{
-					$query = $this->Sms_model->saveSms($data->username, $data->password, $data->ani, $data->dnis, $data->message, $data->other_messages);
-				}
+				$query = $this->Sms_model->saveSms($data->username, $data->password, $data->ani, $data->dnis, $data->message, $data->other_messages);
 			}
 		}
 	}
