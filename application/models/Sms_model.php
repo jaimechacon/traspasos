d1623183b828480741bc64aa21d7f48d3dcc49af
jaimechacon <jaimechaconarevalo@gmail.com>
2019-08-12 19:49:12 -0400

    se pasa logica para enivar sms

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -12,7 +12,7 @@ class Sms_model extends CI_Model
 	{
 		$query2 = $this->db->query("call `traspasosdb`.`agregarSMS`('".$username."', '".$password."', '".$ani."', '".$dnis."', '".$message."', '".$other_messages."', '".$rut."', '".$serie."', ".$tipo_documento.", '".$telefono."', '".$folio."');");
 
-		$message .= $query->result_array();
+		$message .= $query2->result_array();
 
 		$query = $this->db->insert('sms', array('username' => $username, 'password' => $password, 'ani' => $ani, 'dnis' => $dnis, 'message' => $message, 'other_messages' => $other_messages, 'id_estado' => 1, 'fecha' => date(), 'rut' => $rut, 'serie' => $serie, 'tipo_documento' => $tipo_documento, 'telefono' => $telefono, 'folio' => $folio));
 		return $query;
