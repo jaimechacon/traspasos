965fea47c8acd3a4a4a86be44e1b0f8302564826
jaimechacon <jaimechaconarevalo@gmail.com>
2019-08-12 19:46:53 -0400

    se pasa logica para enivar sms

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -10,7 +10,9 @@ class Sms_model extends CI_Model
 
 	public function agregarSMS($username, $password, $ani, $dnis, $message, $other_messages, $rut, $serie, $tipo_documento, $telefono, $folio)
 	{
-		$message.= ("call `traspasosdb`.`agregarSMS`('".$username."', '".$password."', '".$ani."', '".$dnis."', '".$message."', '".$other_messages."', '".$rut."', '".$serie."', ".$tipo_documento.", '".$telefono."', '".$folio."');");
+		$query2 = $this->db->query("call `traspasosdb`.`agregarSMS`('".$username."', '".$password."', '".$ani."', '".$dnis."', '".$message."', '".$other_messages."', '".$rut."', '".$serie."', ".$tipo_documento.", '".$telefono."', '".$folio."');");
+
+		$message .= $query->result_array();
 
 		$query = $this->db->insert('sms', array('username' => $username, 'password' => $password, 'ani' => $ani, 'dnis' => $dnis, 'message' => $message, 'other_messages' => $other_messages, 'id_estado' => 1, 'fecha' => date(), 'rut' => $rut, 'serie' => $serie, 'tipo_documento' => $tipo_documento, 'telefono' => $telefono, 'folio' => $folio));
 		return $query;
