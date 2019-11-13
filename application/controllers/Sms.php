69da5f5e66b591051bc28139d435dabf85bf9aaa
jaimechacon <jaimechaconarevalo@gmail.com>
2019-11-13 17:14:11 -0300

    se sube url de servicio previred produccion

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -124,16 +124,16 @@ class Sms extends CI_Controller {
 				$post .= '<peticion llave="ProvidaPrevired2019">';
 				$post .= '<peticionservicio tipo="AUT">';
 				$post .= '<parametro nombre="usuario" valor="76265736-8" />';
-				$post .= '<parametro nombre="password" valor="ProvidaPrevired2019" />';
+				$post .= '<parametro nombre="password" valor="Provida" />'; //ProvidaPrevired2019" />';
 				$post .= '</peticionservicio>';
 				$post .= '<peticionservicio tipo="CAF">';
-			    $post .= '<parametro nombre="rut" valor="9206896K" />';//.$rut_afiliado.'" />';
+			    $post .= '<parametro nombre="rut" valor="'.$rut_afiliado.'" />';
 			    $post .= '<parametro nombre="periodo" valor="'.$periodo.'" />';
 		        $post .= '</peticionservicio>';
 		     	$post .= '</peticion>';
 
-		     	$cliente = new nusoap_client("https://qagintegracion.previred.com/wIntegracion/axis/services/MonitorPrevired?wsdl", true);
-		     	//$cliente = new nusoap_client("https://wbackend.previred.com/axis/services/MonitorPrevired?wsdl", true);
+		     	//$cliente = new nusoap_client("https://qagintegracion.previred.com/wIntegracion/axis/services/MonitorPrevired?wsdl", true);
+		     	$cliente = new nusoap_client("https://wbackend.previred.com/axis/services/MonitorPrevired?wsdl", true);
 		     	$array_ws = array('xml' => $post);
 		     	$respuesta = $cliente->call('ejecuta', array('xml' => $post));
 
