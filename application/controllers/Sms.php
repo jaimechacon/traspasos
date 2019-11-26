fff98d263a37cda33db56432c9ab441906ba03ff
jaimechacon <jaimechaconarevalo@gmail.com>
2019-11-26 13:55:18 -0300

    se suben cambios

--- application/controllers/Sms.php
+++ application/controllers/Sms.php
@@ -123,7 +123,7 @@ class Sms extends CI_Controller {
 				$post = '<?xml version="1.0" encoding="ISO-8859-1" ?>';
 				$post .= '<peticion llave="2vUR4BV2iS">';
 				$post .= '<peticionservicio tipo="AUT">';
-				$post .= '<parametro nombre="usuario" valor="76.391.999-4" />';
+				$post .= '<parametro nombre="usuario" valor="76391999-4" />';
 				$post .= '<parametro nombre="password" valor="2vUR4BV2iS" />'; //ProvidaPrevired2019" />';
 				$post .= '</peticionservicio>';
 				$post .= '<peticionservicio tipo="CAF">';
@@ -141,7 +141,7 @@ class Sms extends CI_Controller {
 
 				//mysqli_next_result($this->db->conn_id);
 				//$resultado = $this->Sms_model->agregarLog($usuario['id_usuario'], $id_sms, $simpleXml);
-				//var_dump($simpleXml);
+				var_dump($simpleXml);
 				$cant = 0;
 				$cantNod = 0;
 
