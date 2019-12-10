697779cf69fedfc10bfe350570c029d5b05f8aec
jaimechacon <jaimechaconarevalo@gmail.com>
2019-12-10 16:12:39 -0300

    se publican los nuevos usuarios

--- application/views/temp/header.php
+++ application/views/temp/header.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset="utf-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Sistema de Reportes Minsal</title>
+    <title>Certificaci&oacute;n OT</title>
 
     <!-- Core CSS - Include with every page -->
 	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
