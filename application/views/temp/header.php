5b2b4bfc152645ca92934f1371efca56246040e7
jaimechacon <jaimechaconarevalo@gmail.com>
2019-12-10 16:17:49 -0300

    se publican los nuevos usuarios

--- application/views/temp/header.php
+++ application/views/temp/header.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset="utf-8" >
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Certificaci&oacute;n OT</title>
 
